5df6839db571f7d7545c121109990e83e592ad50
Andrew Garfield <Andrew.Garfield@citrix.com>
2013-09-04 13:42:17 -0400

    Updated comments.

--- lib/siriproxy-vera.rb
+++ lib/siriproxy-vera.rb
@@ -83,12 +83,13 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
       end
     end
   end
-    
+  
+  # Created a simplified function to pull variable information from vera.  
   def get_variable(device, variable)
     @client.get("#{@base_uri}/data_request", {:id => "variableget", "Variable" => variable}.merge(device)).content
   end
   
-  # vera:49451/data_request?id=action&serviceId=urn:micasaverde-com:serviceId:AlarmPartition2&DeviceNum=20&RequestArmMode=Disarmed&PINCode=2623
+  # Created a simplified function to perform actions on a vera system.
   def perform_action(device, action, variable, set_to)
     @client.get("#{@base_uri}/data_request", {:id => "action", :action => action, variable.to_sym => set_to}.merge(device)).content
   end
@@ -106,12 +107,14 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
     end
   end 
   
+  # a good listen function just for testing siriproxy-vera.
   listen_for /how many scenes do you know/i do
     say "I know about #{@scenes.size} scenes."
 
     request_completed
   end
 
+  # Reloads the device and scene information from vera through a listen command.
   listen_for /reload device information/i do
     response = reload_from_vera
     say response, spoken: "Okay, I have reloaded the configuration from Vera."
@@ -119,12 +122,12 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
     request_completed
   end
   
+  # Turns on or off a binary or dimmable light.
   listen_for /turn ([a-z]*) the ([\d\w\s]*)/i do |on_or_off, input|
-    lights = @binary_lights.merge(@dimmable_lights)
+    lights = @binary_lights.merge(@dimmable_lights) #merge the binary and dimmabe light hashes.
     
-    #say "I undestood #{input}"
-    if lights.has_key?(input.downcase)
-      result = turn(lights[input.downcase], on_or_off)
+    if lights.has_key?(input.downcase) # Search the keys in the lights hash for a match to the input.
+      result = turn(lights[input.downcase], on_or_off) # Perform the action to turn on/off the lights.
       say "Turning #{input.downcase} #{on_or_off}." if result
       say "Error turning #{input.downcase} #{on_or_off}." if not result
     else
@@ -134,11 +137,12 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
     request_completed
   end
   
+  # Turns your alarm partition to away (or more technically "Armed") mode.
   listen_for /(I|We) (am|are) leaving/ do
-    if @alarm
-      arm_mode = get_variable(@alarm, "ArmMode")
+    if @alarm # Ensures that siriproxy-vera found an alarm panel from your system.
+      arm_mode = get_variable(@alarm, "ArmMode") 
       if arm_mode == "Disarmed"
-        request = perform_action(@alarm, "RequestArmMode", "State", "Armed")
+        request = perform_action(@alarm, "RequestArmMode", "State", "Armed") # Runs call to arm the system.
         say "Be Safe!  See you soon!", :spoken => "Okay, I'll prepare the house for you." if request
         say "Sorry but something went wrong." if not request
       else
@@ -151,11 +155,12 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
     request_completed
   end
   
+  # Turns your alarm partition to Stay mode.
   listen_for /(I|We) (am|are) staying in/ do
-    if @alarm
+    if @alarm # Ensures that siriproxy-vera found an alarm panel from your system.
       arm_mode = get_variable(@alarm, "ArmMode")
       if arm_mode == "Disarmed"
-        request = perform_action(@alarm, "RequestArmMode", "State", "Stay")
+        request = perform_action(@alarm, "RequestArmMode", "State", "Stay")  # Runs call to arm the system.
         say "Okay, I will arm the house for you." if request
         say "Sorry but something went wrong." if not request
       else
@@ -169,11 +174,11 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
   end
   
   
-  # listen_for /set level ([0-9,]*[0-9]) on ([\d\w\s]*)/i do |number,input|
+  # Listen command to change the light level of a dimmable light
   listen_for /change ([\d\w\s]*)/i do |input|
-    if @dimmable_lights.has_key?(input.downcase)
+    if @dimmable_lights.has_key?(input.downcase) # Search the keys in the @dimmable_lights hash for a match to the input.
       number = ask "To what should I change #{input.downcase} to?"
-      if (number =~ /([0-9,]*[0-9])/i) and ((number.to_i <= 100) and (number.to_i >= 0))
+      if (number =~ /([0-9,]*[0-9])/i) and ((number.to_i <= 100) and (number.to_i >= 0)) # Ask for additional input the dim level.
         result = turn_dimmable(@dimmable_lights[input.downcase], number)
         say "Turning #{input.downcase} to #{number} percent." if result
         say "Error turning #{input.downcase} to #{number} percent." if not result
@@ -185,9 +190,9 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
     request_completed
   end
   
+  # listen command to run a particular scene.
   listen_for /set (scene|seen|seem) ([\d\w\s]*)/i do |spacer,input|
-    #say "I undestood #{input}"
-    if @scenes.has_key?(input.downcase)
+    if @scenes.has_key?(input.downcase) # Search the keys in the @scenes hash for a match to the input.
       result = @client.get("#{@base_uri}/data_request",
       {:id => "lu_action", 
         :serviceId => "urn:micasaverde-com:serviceId:HomeAutomationGateway1", 
@@ -201,82 +206,4 @@ class SiriProxy::Plugin::Vera < SiriProxy::Plugin
 
       request_completed
   end
-
-  # #get the user's location and display it in the logs
-#   #filters are still in their early stages. Their interface may be modified
-#   filter "SetRequestOrigin", direction: :from_iphone do |object|
-#     puts "[Info - User Location] lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
-# 
-#     #Note about returns from filters:
-#     # - Return false to stop the object from being forwarded
-#     # - Return a Hash to substitute or update the object
-#     # - Return nil (or anything not a Hash or false) to have the object forwarded (along with any
-#     #    modifications made to it)
-#   end
-# 
-#   listen_for /where am i/i do
-#     say "Your location is: #{location.address}"
-#   end
-# 
-#   listen_for /test siri proxy/i do
-#     say "Siri Proxy is up and running!" #say something to the user!
-# 
-#     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-#   end
-# 
-#   #Demonstrate that you can have Siri say one thing and write another"!
-#   listen_for /you don't say/i do
-#     say "Sometimes I don't write what I say", spoken: "Sometimes I don't say what I write"
-#   end
-# 
-#   #demonstrate state change
-#   listen_for /siri proxy test state/i do
-#     set_state :some_state #set a state... this is useful when you want to change how you respond after certain conditions are met!
-#     say "I set the state, try saying 'confirm state change'"
-# 
-#     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-#   end
-# 
-#   listen_for /confirm state change/i, within_state: :some_state do #this only gets processed if you're within the :some_state state!
-#     say "State change works fine!"
-#     set_state nil #clear out the state!
-# 
-#     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-#   end
-# 
-#   #demonstrate asking a question
-#   listen_for /siri proxy test question/i do
-#     response = ask "Is this thing working?" #ask the user for something
-# 
-#     if(response =~ /yes/i) #process their response
-#       say "Great!"
-#     else
-#       say "You could have just said 'yes'!"
-#     end
-# 
-#     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-#   end
-# 
-#   #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
-#   listen_for /siri proxy number ([0-9,]*[0-9])/i do |number|
-#     say "Detected number: #{number}"
-# 
-#     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-#   end
-# 
-#   #demonstrate injection of more complex objects without shortcut methods.
-#   listen_for /test map/i do
-#     add_views = SiriAddViews.new
-#     add_views.make_root(last_ref_id)
-#     map_snippet = SiriMapItemSnippet.new
-#     map_snippet.items << SiriMapItem.new
-#     utterance = SiriAssistantUtteranceView.new("Testing map injection!")
-#     add_views.views << utterance
-#     add_views.views << map_snippet
-# 
-#     #you can also do "send_object object, target: :guzzoni" in order to send an object to guzzoni
-#     send_object add_views #send_object takes a hash or a SiriObject object
-# 
-#     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-#   end
 end
